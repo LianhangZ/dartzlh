88ac5f0ac71246ec0d4781515be5d82eed9b21ab
LianhangZ <lz481456@gmail.com>
2023-11-25 15:41:49 +0800

    failed try for future and File

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# dartzlh
+# dart
+# Mixins
+- Mixins are a way of defining code that can be reused in multiple class hierarchies.
+# Asynchronous programming: futures, async, await
+同时处理两个及以上的事物。
+```
+
+```
--- demon.dart
+++ demon.dart
@@ -1,3 +1,36 @@
-main(){
-    print('hello dart');
+// void main(){
+//     print('hello dart');
+// }
+
+
+
+// import 'dart:io';
+
+// void main() async{
+//     var config = File('config.txt');
+
+//     // Put the whole file in a single string.
+//     var stringContents = await config.readAsString();
+//     print('The file is ${stringContents.length} characters long.');
+
+//     // Put each line of the file into its own string.
+//     var lines = await config.readAsString();
+//     print('The file is ${lines.length} lines long.');
+// }
+
+
+import 'dart:io';
+
+void main(){
+    String fileName = 'test.txt';
+    readFile(fileName).then((value) {
+        print(value);
+    },onError:(e){
+        print('文件不存在');
+    });
+}
+
+Future<String> readFile(String fileName){
+    File file = File(fileName);
+    return file.readAsString();
 }
